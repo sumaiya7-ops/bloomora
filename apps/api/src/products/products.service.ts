939e526520933f5689c43e595d33dad636e32046
sumaiya7-ops <sumaiya7-ops@gmail.com>
2026-06-05 20:02:03 +0600

    add file

--- apps/api/src/products/products.service.ts
+++ apps/api/src/products/products.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Product, ProductDocument } from './schemas/product.schema';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(
+    @InjectModel(Product.name)
+    private productModel: Model<ProductDocument>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto, userId: string) {
+    const product = await this.productModel.create({
+      ...createProductDto,
+      createdBy: userId,
+    });
+
+    return product;
+  }
+
+  async findAll() {
+    return this.productModel.find();
+  }
+}
